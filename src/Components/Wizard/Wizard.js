14bcf1e7c4160885dab94804a0200012276190eb
grayluker05 <graysonluker@gmail.com>
2019-10-18 12:18:22 -0600

    third commit

--- src/Components/Wizard/Wizard.js
+++ src/Components/Wizard/Wizard.js
@@ -3,6 +3,24 @@ import React, {Component} from 'react';
 class Wizard extends Component {
     constuctor(){
         super();
+
+        this.state = {
+            property_name: '',
+            address: '',
+            city: '',
+            state: '',
+            zip: '',
+            image_url: '',
+            morgage: '',
+            rent: ''
+        };
+    }
+
+    handleInput = (e) => {
+        let {name, value} = e.target;
+        this.setState({
+            [name]: value
+        });
     }
 
     render(){
@@ -12,6 +30,41 @@ class Wizard extends Component {
                 <Link to = {{pathname: '/'}}>
                     <button>Cancel</button>
                 </Link>
+
+                <input 
+                name = 'property_name'
+                value = {property_name}
+                onChange={e => this.handleInput(e)}
+                placeholder='Address'
+                />
+
+                <input 
+                name = 'address'
+                value = {address}
+                onChange={e => this.handleInput(e)}
+                placeholder='Address'
+                />
+
+                <input 
+                name = 'city'
+                value = {city}
+                onChange={e => this.handleInput(e)}
+                placeholder='City'
+                />
+
+                <input 
+                name='state'
+                value={state}
+                onChange={e => this.handleInput(e)}
+                placeholder='State'
+                />
+
+                <input 
+                name='zip'
+                value={zip}
+                onChange={e => this.handleInput(e)}
+                placeholder='Zip'
+                />
             </div>
         )
     }
